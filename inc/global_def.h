c55abebe401601606325ace52977c632b9878b36
icingworld <984442022@qq.com>
2023-10-11 17:34:06 +0800

    fix: global definition file

--- inc/global_def.h
+++ inc/global_def.h
@@ -1,6 +1,45 @@
-#ifndef _GLOBAL_DEF_H_
-#define _GLOBAL_DEF_H_
+#ifndef GLOBAL_DEF_H
+#define GLOBAL_DEF_H
 
+#include <QPixmap>
+#include <QDir>
+#include <QString>
+#include <QVector>
+#include <QColor>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QSlider>
+#include <QLabel>
+#include <QLineEdit>
+#include <QCursor>
+#include <QMouseEvent>
+#include <QDebug>
+#include <QPainter>
+#include <QIntValidator>
 
+/* custom directory */
+#define     OUT_DIR         "out"
+#define     LOG_DIR         "log"
+#define     HISTORY_DIR     "history"
+#define     TEST_DIR        "test"
 
-#endif
+/* customs param */
+#define     IMG_WIDHT   120
+#define     IMG_HEIGHT  90
+
+/* custom struct */
+typedef struct
+{
+    int x;
+    int y;
+} Coord;
+
+typedef struct
+{
+    int width;
+    int height;
+} Size;
+
+#endif // GLOBAL_H
