698f7163227f71e29180a04dbb240870ceeb8c46
icingworld <984442022@qq.com>
2023-10-12 17:18:37 +0800

    fix: canvas

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -1,19 +1,21 @@
 #include "canvas.h"
 
-Canvas::Canvas(int width, int height, QWidget * parent)
+Canvas::Canvas(int width, int height, QColor & color, QWidget * parent)
     : QWidget(parent),
       x_width(width),
       x_height(height),
+      curColor(color),
       x_vector(x_height * x_width)
 {
     x_layout = new QGridLayout(this);
     for (int i=0;i < x_height;i++) {
         for (int j=0;j < x_width;j++) {
-            qDebug() << x_width*i+j;
-            x_vector[x_width*i+j] = new PixelBlock();
+            // qDebug() << x_width*i+j;
+            x_vector[x_width*i+j] = new PixelBlock(color);
             x_layout->addWidget(x_vector[x_width*i+j], i, j);
         }
     }
+    x_layout->setSpacing(0);
     setLayout(x_layout);
 }
 
@@ -22,3 +24,13 @@ Canvas::~Canvas()
     delete x_layout;
 }
 
+void Canvas::setSize(int length)
+{
+    qDebug() << "get length: " << length;
+    for (int i=0;i < x_height;i++) {
+        for (int j=0;j < x_width;j++) {
+            x_vector[x_width*i+j]->resize(length, length);
+        }
+    }
+    update();
+}
--- src/canvas.h
+++ src/canvas.h
@@ -8,18 +8,20 @@
 class Canvas : public QWidget
 {
 public:
-    Canvas(int, int, QWidget * parent = nullptr);
+    Canvas(int, int, QColor &, QWidget * parent = nullptr);
     virtual ~Canvas();
 
 private:
     int x_width;
     int x_height;
+    QColor & curColor;
 
 public:
     QGridLayout * x_layout;
     QVector <PixelBlock *> x_vector;
-
-
+    
+public slots:
+    void setSize(int);
 };
 
 #endif // CANVAS_H
