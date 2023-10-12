5b9a2baa5a65408ffc9d60e5f59a5674d15062d4
icingworld <984442022@qq.com>
2023-10-12 17:19:30 +0800

    fix: pixelblock

--- src/pixelblock.cpp
+++ src/pixelblock.cpp
@@ -1,13 +1,14 @@
 #include "pixelblock.h"
 
-PixelBlock::PixelBlock(QWidget * parent)
-    : QLabel(parent)
+PixelBlock::PixelBlock(QColor & color, QWidget * parent)
+    : QLabel(parent),
+      curColor(color)
 {
-
+    setFrameShape(QFrame::Box);
+    setStyleSheet("background-color:rgb(209,209,209);border-width:1px;border-style:solid;border-color:rgb(255, 255, 255);");
 }
 
 PixelBlock::~PixelBlock()
 {
 
 }
-
--- src/pixelblock.h
+++ src/pixelblock.h
@@ -8,8 +8,24 @@ class PixelBlock : public QLabel
     Q_OBJECT
 
 public:
-    PixelBlock(QWidget * parent = nullptr);
+    PixelBlock(QColor & color, QWidget * parent = nullptr);
     virtual ~PixelBlock();
+
+private:
+    QColor & curColor;
+    void setBackground() {
+        setFrameShape(QFrame::NoFrame);
+        setStyleSheet(QString("background:rgb(%1,%2,%3);").arg(curColor.red()).arg(curColor.green()).arg(curColor.blue()));
+    }
+
+protected:
+    void mousePressEvent(QMouseEvent * event) override {
+        if (event->button() == Qt::LeftButton) {
+            setBackground();
+        }
+        QWidget::mousePressEvent(event);
+    }
+
 };
 
 #endif // PIXELBLOCK_H
